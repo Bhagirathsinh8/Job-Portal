045afc9cad4b86155605077055d09351d8157c0e
Bhagirathsinh8 <bhagirathnakum8@gmail.com>
2025-08-05 12:20:46 +0530

    Update multer.js

--- Backend/src/middleware/multer.js
+++ Backend/src/middleware/multer.js
@@ -3,12 +3,12 @@ const path = require("path");
 const fs = require("fs");
 
 // Absolute path to the uploads folder (outside src/)
-const uploadPath = path.join(__dirname, "../../uploads");
+// const uploadPath = path.join(__dirname, "../../uploads");
 
 // Automatically create the uploads folder if it doesn't exist
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
+// if (!fs.existsSync(uploadPath)) {
+//   fs.mkdirSync(uploadPath, { recursive: true });
+// }
 
 // Multer storage config for local storage
 // const localstorage = multer.diskStorage({
